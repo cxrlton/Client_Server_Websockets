53285d36b06590e63bd818202d85cc9e3172adc6
Pradham Mummaleti <118067855+cxrlton@users.noreply.github.com>
2023-08-31 19:12:38 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,19 +1,19 @@
-# Importing the relevant libraries
+
 import websockets
 import asyncio
 
-# The main function that will handle connection and communication 
+# This function will handle connection and communication 
 # with the server
 async def listen():
     url = "ws://127.0.0.1:6503"
     # Connect to the server
     async with websockets.connect(url) as ws:
-        # Send a greeting message
-        await ws.send("Hello Server!")
-        # Stay alive forever, listening to incoming msgs
+        # Send intended message
+        await ws.send("Ping me back.")
+        # creates a forever loop 
         while True:
             msg = await ws.recv()
             print(msg)
 
-# Start the connection
+# Starts the connection
 asyncio.get_event_loop().run_until_complete(listen())
